5ae1c7cda113247142f36b105b83e911a7b22968
Ayaan-Hashmi <apex.hashmi@gmail.com>
2024-08-07 22:21:15 +0530

    Update main.py with changes

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
 
-def makeCommits (days : int):
+def makeCommits(days: int) -> int:
     if days < 1:
         os.system('git push')
+        return 1  # Base case should return an integer
     else:
         dates = f"{days} days ago"
         with open('data.txt', 'a') as file:
             file.write(f'{dates} <- this was the commit for the day!!\n')
-        
-        # staging 
+
+        # staging
         os.system('git add data.txt')
 
-        # commit 
-        os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
+        # commit
+        os.system(f'git commit --date="{dates}" -m "First commit for the day!"')
 
-        return days * makeCommits(days - 1)
+        return makeCommits(days - 1)  # Recursive case should return the result of the next call
 
-makeCommits(1)
+# Call the function
+makeCommits(365)
